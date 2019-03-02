085cb0dcb0b3ee2f9fa466d7cf2e1a5f1d79047f
bit9tream <megapixel483@gmail.com>
2019-03-02 10:28:48 +0300

    0.2

--- delost.py
+++ delost.py
@@ -6,6 +6,8 @@ from telebot.types import Message
 TOKEN = '583885451:AAFNXpsPeAbt8dNLwwuKoYwhSdopzb8KVSY'
 bot = telebot.TeleBot(TOKEN)
 ADDSTICK = False
+ADMINS = []
+CREATOR = '564932461'
 STICKERS = ['CAADAgADHwADa-18CnJ6NnqU84-1Ag',
             'CAADAgADyQEAAtvnDQAB19KJ0eCiO5cC',
             'CAADAgADVQAD6kb8ECvohJYCchf_Ag',
@@ -20,32 +22,59 @@ STICKERS = ['CAADAgADHwADa-18CnJ6NnqU84-1Ag',
             'CAADAQADhwEAAjqtBAUdAR6f6eANlwI']
 USERS = set()
 @bot.message_handler(commands=['start'])
-def command_handler(message: Message):
+def command_start(message: Message):
     bot.send_message(message.chat.id, 'Привет, рад тебя видеть\nНапиши /help чтобы получить справку')
+    print(message.from_user.id)
 
 @bot.message_handler(commands=['help'])
-def command_handler(message: Message):
+def command_help(message: Message):
     bot.send_message(message.chat.id, 'Итак, вот твой чек:\n /start - для начала\n /help - помощь\n /getstat -  получить статистику\n /addsticker - добавить стикер в пресет\n')
 
 @bot.message_handler(commands=['hello'])
-def command_handler(message: Message):
+def command_hello(message: Message):
     bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name)
 
+#TODO
+@bot.message_handler(commands=['requestadmin'])
+def command_setadmin(message: Message):
+    bot.send_message(message.chat.id, 'Ожидайте одобрения от одного из управленцев...')
+    bot.send_message(CREATOR, f'Заявка в админы от {message.from_user.username}  \nДля одобрения отправь /setadmin')
+
+@bot.message_handler(commands=['adminlist'])
+def command_adminlist(message: Message):
+    bot.send_message(message.chat.id, ADMINS)
+
 @bot.message_handler(commands=['getstat'])
-def command_handler(message: Message):
+def command_getstat(message: Message):
     bot.reply_to(message, 'В процессе...')
 
 @bot.message_handler(commands=['addsticker'])
-def command_handler(message: Message):
+def command_addsticker(message: Message):
     global ADDSTICK
     # bot.reply_to(message, 'В процессе...')
     bot.send_message(message.chat.id, 'Отправь мне свой стикер')
     ADDSTICK = True
 
+@bot.message_handler(commands=['stickbomb'])
+def command_stickbomb(message: Message):
+    i = 100
+    while i > 0:
+        bot.send_sticker(message.chat.id, random.choice(STICKERS))
+        i -= 1
+
+@bot.message_handler(commands=['ddos'])
+def command_stickbomb(message: Message):
+    i = 1000
+    while i > 0:
+        bot.send_sticker(message.chat.id, random.choice(STICKERS))
+        bot.send_message(message.chat.id, 'DDOS')
+        i -= 1
+
 @bot.message_handler(content_types=['text'])
 def echo_digits(message: Message):
     if message.from_user.id not in USERS:
         USERS.add(message.from_user.id)
+    print(message)
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_handler(message: Message):
@@ -62,4 +91,3 @@ def sticker_handler(message: Message):
 
 
 bot.polling(timeout=60)
-#@bot.edited_message_handler(content_types=['text'])
---
+++ git.sh
@@ -0,0 +1,1 @@
+git add delost.py && git commit -m "fix" && git push heroku master
